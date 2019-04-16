a2cf560f351ce521f34a9319b3b323bd87660635
moseley <jeremymoseley@me.com>
2019-04-16 18:08:11 +0900

    Check CORS using only function def

--- evolution/read.js
+++ evolution/read.js
@@ -26,11 +26,6 @@ module.exports.read = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
       body: JSON.stringify(result.Item),
     };
     callback(null, response);
